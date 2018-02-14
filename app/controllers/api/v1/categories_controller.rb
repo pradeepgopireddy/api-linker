c2ed34f61f6fbabe0283ff0e8dbb4e341937310e
Pradeep Gopireddy <pradeepgopireddy456@gmail.com>
2018-02-14 09:40:59 +0530

    added index action in categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,3 +1,8 @@
 class Api::V1::CategoriesController < Api::V1::ApiController
 
+	#localhost:3000/api/v1/categories - GET
+	def index
+		@categories = Category.all
+		render json: @categories
+	end
 end
