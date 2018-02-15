bc18305cffc37a2b672116949bdf499a058fd7e4
Pradeep Gopireddy <pradeepgopireddy456@gmail.com>
2018-02-15 07:26:45 +0530

    reviews index action added via nested routes

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,2 +1,9 @@
 class Api::V1::ReviewsController < Api::V1::ApiController
+
+	#localhost:3000/api/v1/restaurants/restaurant.id/reviews
+	def index
+		@restaurant = Restaurant.find(params[:restaurant_id])
+		@reviews = @restaurant.reviews
+		render json: @reviews
+	end
 end
